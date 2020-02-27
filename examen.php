f528631e167d74ce87631ed0968f4dfc36b60765
lizbeth-pozos-zey <56325021+lizbeth-pozos-zey@users.noreply.github.com>
2020-02-27 14:55:49 -0600

    ex

---
+++ examen.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Examen</title>
+</head>
+<body>
+	<?php  //se seleccionan los valores para determinar la multiplicacion?>
+<form action="examen_datos.php" method="POST">
+	<p>SELECCIONE LOS NUMEROS A MULTIPLICAR</p> 
+	<hr>
+	<p>NÚMERO</p>
+	<select name="num">
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
+	<option>4</option>
+	<option>5</option>
+	<option>6</option>
+	<option>7</option>
+	<option>8</option>
+	<option>9</option>
+	<option>10</option>
+	<option>11</option>
+	<option>12</option>
+	</select>
+<br>
+<p>MULTIPLICADOR</p>
+<select name="nume2">
+	<option>1</option>
+	<option>2</option>
+	<option>3</option>
+	<option>4</option>
+	<option>5</option>
+	<option>6</option>
+	<option>7</option>
+	<option>8</option>
+	<option>9</option>
+	<option>10</option>
+	<option>11</option>
+	<option>12</option>
+	</select>
+    <input type="submit">
+
+</form>
+</body>
+</html>
---
+++ examen_datos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>LOS NUMEROS MULTIPLICADOS SON:</title>
+</head>
+<body>
+<?php 
+//se declaran las variables con  los numeros ya ingresados en la pagina anterior (principal)
+
+$num1 =  $_POST['num'];
+$num2 =  $_POST['nume2'];
+
+$res= $num1*$num2 //se realiza la multiplicacion de los datos obtenidos
+
+ echo "EL NUMERO ES " $num1 ;
+ echo "EL MULTIPLICADOR " $num2;
+ echo " EL RESULTADO " $res;
+ ?>
+</body>
+</html>
